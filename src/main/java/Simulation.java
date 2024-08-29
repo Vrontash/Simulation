195850a4a29e280c92fe88d606e1f456ff307153
Vlad <vladmelnikov078@gmail.com>
2024-08-29 23:53:43 +0300

    feat: add turn-based and real-time simulations

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,14 +1,15 @@
 package src.main.java;
 
 import src.main.java.actions.*;
-
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class Simulation {
     private static final int X_MAX = 10;
     private static final int Y_MAX = 10;
-    private static final int HERBIVORE_NUM = 4;
+    private static final int HERBIVORE_NUM = 6;
     private static final int PREDATOR_NUM = 2;
     private static final int GRASS_NUM = 11;
     private static final int ROCK_NUM = 6;
@@ -18,6 +19,7 @@ public class Simulation {
     private final SimCLI cli;
     private final ArrayList<Action> initActions;
     private final ArrayList<Action> turnActions;
+    private boolean isSimOver = false;
     Simulation(){
         this.map = new Map(X_MAX, Y_MAX);
         this.moveCount = 0;
@@ -25,7 +27,14 @@ public class Simulation {
         this.initActions = new ArrayList<>();
         this.turnActions = new ArrayList<>();
     }
-    public void nextTurn(){}
+    public void nextTurn(){
+        cli.renderTurn(map, moveCount);
+        Set<Coordinates> entitiesCoordinates = Set.copyOf(map.coordinatesSet());
+        for (Action act : turnActions)
+            act.perform(map);
+        if (entitiesCoordinates.equals(map.coordinatesSet())) isSimOver = true;
+        moveCount ++;
+    }
     public void startSim(){
         cli.prepMenu();
         createActions();
@@ -50,10 +59,24 @@ public class Simulation {
         }
     }
     public void playSimTurnBased(){
-        cli.renderTurn(map);
+        Scanner sc = new Scanner(System.in);
+        while(!isSimOver) {
+            nextTurn();
+            System.out.println("Press Enter to continue");
+            sc.nextLine();
+        }
+        cli.renderEndMessage(moveCount);
     }
     public void playSimRealTime(){
-        cli.renderTurn(map);
+        while(!isSimOver) {
+            nextTurn();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        cli.renderEndMessage(moveCount);
     }
     public void createActions(){
         initActions.add(new GrassSpawnAction(GRASS_NUM));
@@ -62,8 +85,6 @@ public class Simulation {
         initActions.add(new HerbivoreSpawnAction(HERBIVORE_NUM));
         initActions.add(new PredatorSpawnAction(PREDATOR_NUM));
 
-//        turnActions.add(new CreatureMoveAction());
+        turnActions.add(new CreatureMoveAction());
     }
-    public void pauseSim(){}
-
 }
