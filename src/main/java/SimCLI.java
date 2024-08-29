c800c948ee1a486c779d196709707d36eb6ee87f
Vlad <vladmelnikov078@gmail.com>
2024-08-29 23:51:33 +0300

    feat: add turn count and end message in cli

--- src/main/java/SimCLI.java
+++ src/main/java/SimCLI.java
@@ -6,9 +6,9 @@ public class SimCLI {
         System.out.println("SIMULATION");
         System.out.println("To enter simulation in turn-based mode press T");
         System.out.println("To enter real-time simulation press R");
-        System.out.println("    To pause simulation in real-time mode press P");
     }
-    public void renderTurn(Map map){
+    public void renderTurn(Map map, int moveCount){
+        System.out.println("Turn " + moveCount);
         for (int y = 0; y < map.yMax; y++) {
             for (int x = 0; x < map.xMax; x++) {
                 Coordinates coord = new Coordinates(x, y);
@@ -19,5 +19,9 @@ public class SimCLI {
             }
             System.out.println();
         }
+    }
+    public void renderEndMessage(int moveCount){
+        System.out.println("Simulation Has Ended!");
+        System.out.println("Simulation was completed in " + moveCount + " turns");
     }
 }
