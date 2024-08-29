483e18035dfd3751600dc9fbfbf33c9fe019e50d
Vlad <vladmelnikov078@gmail.com>
2024-08-29 17:44:28 +0300

    Add BreadthFirstSearch pathfinding for creatures

---
+++ src/main/java/Pathfinding.java
@@ -0,0 +1,78 @@
+package src.main.java;
+
+import src.main.java.simElements.objects.Grass;
+
+import java.util.*;
+
+
+public class Pathfinding {
+    private static List<Coordinates> getObstacle(Map map){
+        List<Coordinates> obstacles =new ArrayList<>();
+        for (Coordinates coord: map.coordinatesSet())
+            if (map.getEntity(coord).getClass() != Grass.class)
+                obstacles.add(coord);
+        return obstacles;
+    }
+    public static List<Coordinates> getNeighbours(Map map, Coordinates coordinates){
+        int x = coordinates.getX();
+        int y = coordinates.getY();
+        List<Coordinates> obstacles = getObstacle(map);
+        obstacles.remove(coordinates);
+        List<Coordinates> neighbours = new ArrayList<>();
+        neighbours.add(new Coordinates(x + 1, y));
+        neighbours.add(new Coordinates(x - 1, y));
+        neighbours.add(new Coordinates(x, y - 1));
+        neighbours.add( new Coordinates(x, y + 1));
+        for (Coordinates obst: obstacles)
+            neighbours.remove(obst);
+        neighbours.removeIf(neigh -> neigh.getX() < 0 || neigh.getX() > map.xMax - 1 || neigh.getY() < 0 || neigh.getY() > map.yMax - 1);
+        return  neighbours;
+    }
+    public static List<Coordinates> BreadthFirstSearch(Map map, Coordinates from, Coordinates to){
+        Queue<Coordinates> unvisited = new LinkedList<>();
+        unvisited.add(from);
+        HashMap<Coordinates, Coordinates> stepHistory = new HashMap<>();
+        stepHistory.put(from, from);
+        while (!unvisited.isEmpty()){
+            Coordinates currentCoord = unvisited.poll();
+            if (currentCoord == to)
+                break;
+            for (Coordinates next: getNeighbours(map, currentCoord))
+                if (!stepHistory.containsKey(next)){
+                    unvisited.add(next);
+                    stepHistory.put(next, currentCoord);
+                }
+        }
+        return reconstructPath(stepHistory, from, to);
+    }
+    public static List<Coordinates> reconstructPath(HashMap<Coordinates, Coordinates> stepHistory, Coordinates from, Coordinates to){
+        Coordinates current = to;
+        List<Coordinates> path = new ArrayList<>();
+        if (!stepHistory.containsKey(to))
+            return List.of();
+        while (current != from){
+            path.add(current);
+            current = stepHistory.get(current);
+        }
+        path.add(from);
+        Collections.reverse(path);
+        return path;
+    }
+    public static Coordinates findNearestObject(Map map, Coordinates from, Class<?> searchedClass){
+        double minDistance = Double.MAX_VALUE;
+        Coordinates nearest = null;
+        for (Coordinates to : map.coordinatesSet()) {
+            if (to.equals(from)) continue;
+            if (map.getEntity(to).getClass() != searchedClass) continue;
+            double distance = findManhattanDistance(from, to);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearest = to;
+            }
+        }
+        return nearest;
+    }
+    public static double findManhattanDistance(Coordinates from, Coordinates to){
+        return Math.abs(from.getX() - to.getX())+ Math.abs(from.getY() - to.getY());
+    }
+}
